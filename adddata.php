015589ca47e5815f145f8410a16d14aabe91249a
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2018-06-12 17:02:54 +0700

    Update adddata.php

--- adddata.php
+++ adddata.php
@@ -9,16 +9,21 @@ $dsn = "pgsql:"
 $db = new PDO($dsn);
 
 $sql = $db->prepare("INSERT INTO IOpoliceNPM ( stationname, postdate, number) VALUES (? ,? ,?)");
-$sql->bindParam(1, 'nawa');
-$sql->bindParam(2, '2018-06-10');
-$sql->bindParam(3, 4);
+$sql->bindParam(1, $name);
+$sql->bindParam(2, $date);
+$sql->bindParam(3, $num);
+
+$name = "nawa";
+$date = "2018-06-10";
+$num = 3;
 echo $sql->execute();
+
+
 
 //$query = "insert into IOpoliceNPM values (2, 'nawa', '2018-06-10',5)";
 //$result = $db->query($query);    
 //print_r($result->fetchAll());
-    
+//$result->closeCursor();
 
-$result->closeCursor();
 ?>
   
