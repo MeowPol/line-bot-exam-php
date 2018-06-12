f93b3d6aa482adbb3153450b21982de814c11297
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2018-06-12 16:52:49 +0700

    Update adddata.php

--- adddata.php
+++ adddata.php
@@ -7,9 +7,16 @@ $dsn = "pgsql:"
     . "sslmode=require;"
     . "password=e3221695be10dad64a793f3949720bc522c81d1f3c71c71d2d53d998b196f5e8";
 $db = new PDO($dsn);
-$query = "insert into IOpoliceNPM values (2, 'nawa', '2018-06-10',5)";
-$result = $db->query($query);    
-    print_r($result->fetchAll());
+
+$sql = $db->prepare("INSERT INTO IOpoliceNPM ( stationname, postdate, number) VALUES (? ,? ,?)");
+$sql->bindParam(1, 'nawa');
+$sql->bindParam(2, '2018-06-10');
+$sql->bindParam(3, 4);
+echo $sql->execute();
+
+//$query = "insert into IOpoliceNPM values (2, 'nawa', '2018-06-10',5)";
+//$result = $db->query($query);    
+//print_r($result->fetchAll());
     
 
 $result->closeCursor();
