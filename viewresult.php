19d27d45f3d465c94a3d0b8f6af1c69ff6af775c
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2018-06-12 11:17:29 +0700

    Update viewresult.php

--- viewresult.php
+++ viewresult.php
@@ -25,7 +25,7 @@ $dsn = "pgsql:"
 $db = new PDO($dsn);
 $query = "select * from IOpoliceNPM";
 $result = $db->query($query);    
-    echo $result;
+    echo ($result->fetchAll());
 /*
 $query = "SELECT employee_id, last_name, first_name, title "
     . "FROM employees ORDER BY last_name ASC, first_name ASC";
