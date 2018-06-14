c459f887ca3471a7e478ede9265b3f11dbc808df
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2018-06-14 19:43:12 +0700

    Update viewresult.php

--- viewresult.php
+++ viewresult.php
@@ -13,7 +13,7 @@
    </thead>
    <tbody>
 <?php
-    
+/*    
 $dsn = "pgsql:"
     . "host=ec2-23-21-129-50.compute-1.amazonaws.com;"
     . "dbname=dfd97o1ehpqpnh;"
@@ -23,13 +23,26 @@ $dsn = "pgsql:"
     . "password=e3221695be10dad64a793f3949720bc522c81d1f3c71c71d2d53d998b196f5e8";
 
 $db = new PDO($dsn);
-
-$month_start = strtotime('first day of this month', time());
-$month_end = strtotime('last day of this month', time());
-    //$d1 = date_format($month_start,"Y-m-d");
-    //$d2 = date_format($month_end,"Y-m-d");
-echo $month_start->format("Y-m-d").'<br/>';
-echo $month_end->format("Y-m-d").'<br/>';
+*/
+    $dt = new DateTime;
+ $dt->setTime(0, 0);
+echo $dt->format('H:i:s').'<br/>';
+ 
+     //$dt1 = clone $dt;
+     
+ $d = $dt->format('Y') . "-" . $dt->format('m') . "-01";    
+ $date1 = date_create($d);
+echo $date1->format('Y m d').'<br/>';
+ $month_end = strtotime('last day of this month', $date1);
+echo $month_end.'<br/>';
+     
+/*     
+ $month_start = strtotime('first day of this month', time());
+ $month_end = strtotime('last day of this month', time());
+ echo date('D, M jS Y', $month_start).'<br/>';
+ echo date('D, M jS Y', $month_end).'<br/>';
+  */  
+    
     
     
 /*
