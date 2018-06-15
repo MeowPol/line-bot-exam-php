56787a23c0ad184ae3756929c4cbc750b1e1c0e7
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2018-06-15 10:19:46 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$str = "ภ.จว.นครพนม\nประจำวันที่ 13 มิ.ย. 2561\nยอดรวม  2  เรื่อง";
+
+$s1 = strpos($str, "ยอดรวม");
+$s2 = strpos($str, "เรื่อง", $s1);
+$str2 = substr($str, $s1+2, $s2-($s1+2));
+echo $str2;
+echo "<br/><br/><br/>";
+$num = preg_replace("/[^0-9]/", '', $str2);
+echo $num;
+echo "<br/><br/><br/>";
+$numio = $num[0];
+
+
+
+
+
+?>
