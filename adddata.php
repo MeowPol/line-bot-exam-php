599525de3e09f28f308dbd1b11b6e2f4348e4f6a
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2018-06-22 14:28:24 +0700

    Update adddata.php

--- adddata.php
+++ adddata.php
@@ -23,8 +23,9 @@ echo $sql->execute();
 
 $query = "delete from IOpoliceNPM where postdate='2018-06-21'";
 $result = $db->query($query);    
-//print_r($result->fetchAll());
-//$result->closeCursor();
+print_r($result->rowCount());
+$result->closeCursor();
+
 
 ?>
   
