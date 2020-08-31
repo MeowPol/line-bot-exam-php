241d0b1681b364fdf9d2e0d885c9acf3d45fe2a6
ladymeaw <39825955+ladymeaw@users.noreply.github.com>
2020-08-31 17:38:38 +0700

    Update viewresult.php

--- viewresult.php
+++ viewresult.php
@@ -6,142 +6,177 @@
 	   <table border=1>
 	   <thead>
 		<tr>
-		 
 <?php
+$id = $_GET["thisid"];
+if(isset($_POST['submit']))
+{
+    $firstName = $_POST['fName'];
+    $lastName = $_POST['lName'];
+    $id = $_POST['id'];
+ echo "<h2>ยินดีต้อนรับ คุณ$firstName $lastName</h2>";
+ echo "id ของท่านคือ $id <br>";
+}else{
+?>
+<form name="test" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+ชื่อ<input type="text" name="fName" size=50><br>
+นามสกุล<input type="text" name="lName" size=50><br>
+<input type="hidden" name="id" value = "<?php echo $id ?>"><br>
+<input type="submit" name="submit" value="ส่งข้อมูล"></td></tr>
+</form>
+<?php
+}
+?>		 
+<?php
+$id = $_GET["thisid"];
+if(isset($_POST['submit']))
+{
+    $firstName = $_POST['fName'];
+    $lastName = $_POST['lName'];
+    $id = $_POST['id'];
+ echo "<h2>ยินดีต้อนรับ คุณ$firstName $lastName</h2>";
+ echo "id ของท่านคือ $id <br>";
+}else{
+?>
+<form name="test" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+เดือน 01-12<input type="text" name="month" size=50><br>
+ปี ค.ศ.<input type="text" name="year" size=50><br>
+<input type="hidden" name="id" value = "<?php echo $id ?>"><br>
+<input type="submit" name="submit" value="ส่งข้อมูล"></td></tr>
+</form>
+<?php
+	$dsn = "pgsql:"
+	    . "host=ec2-23-21-129-50.compute-1.amazonaws.com;"
+	    . "dbname=dfd97o1ehpqpnh;"
+	    . "user=greeojbcxckhvv;"
+	    . "port=5432;"
+	    . "sslmode=require;"
+	    . "password=e3221695be10dad64a793f3949720bc522c81d1f3c71c71d2d53d998b196f5e8";
 
-$dsn = "pgsql:"
-    . "host=ec2-23-21-129-50.compute-1.amazonaws.com;"
-    . "dbname=dfd97o1ehpqpnh;"
-    . "user=greeojbcxckhvv;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=e3221695be10dad64a793f3949720bc522c81d1f3c71c71d2d53d998b196f5e8";
+	$db = new PDO($dsn);
 
-$db = new PDO($dsn);
+		   $dt = date_create();
+		   $dt->setTime(0, 0);
 
-	   $dt = date_create();
-	   $dt->setTime(0, 0);
-			
-		//current month	
-	   //$month_start = date_format($dt,"Y-m-01 H:i:s");
-	   //$month_end = date_format($dt,"Y-m-t H:i:s");
-	   
-			
-	   $month_start = date("Y-m-d H:i:s",strtotime("first day of last month midnight"));
-	   $month_end = date("Y-m-d H:i:s",strtotime("last day of last month midnight"));
-			
-			
-	   //echo $month_start.'<br/>';
-	   //echo $month_end.'<br/>';
+			//current month	
+		   //$month_start = date_format($dt,"Y-m-01 H:i:s");
+		   //$month_end = date_format($dt,"Y-m-t H:i:s");
 
-    
-	   $where = "where postdate between '" . $month_start . "' and '" .$month_end. "' ";
-	   //$query = "select * from IOpoliceNPM ".$where." order by postdate";
-	   $query = "SELECT DISTINCT postdate FROM IOpoliceNPM " .$where . " order by postdate";
-	   //echo $query.'<br/>';
-	   $result = $db->query($query);
-	   $postdatearr = $result->fetchAll(PDO::FETCH_COLUMN, 0);
-	   //print_r($postdatearr);
-	   //echo "<br/><br/>";	   
-	   
-	   $query = "SELECT DISTINCT stationname FROM IOpoliceNPM " .$where . " order by stationname";
-	   //echo $query.'<br/>';
-	   $result = $db->query($query);
-	   $namearr = $result->fetchAll(PDO::FETCH_COLUMN, 0);
-	   //print_r($namearr);
-	   //echo "<br/><br/>";
-	   	   
-	   $array = array();
-			$totalio = array();
-	   for($i=0; $i<count($postdatearr); $i++){
-		   $array[$postdatearr[$i]] = array();
-		   $totalio[$postdatearr[$i]] = 0;
-		   for($j=0; $j<count($namearr); $j++){
-			   $array[$postdatearr[$i]][$namearr[$j]] = 0;
+
+		   $month_start = date("Y-m-d H:i:s",strtotime("first day of last month midnight"));
+		   $month_end = date("Y-m-d H:i:s",strtotime("last day of last month midnight"));
+
+
+		   echo $month_start.'<br/>';
+		   echo $month_end.'<br/>';
+
+
+		   $where = "where postdate between '" . $month_start . "' and '" .$month_end. "' ";
+		   //$query = "select * from IOpoliceNPM ".$where." order by postdate";
+		   $query = "SELECT DISTINCT postdate FROM IOpoliceNPM " .$where . " order by postdate";
+		   //echo $query.'<br/>';
+		   $result = $db->query($query);
+		   $postdatearr = $result->fetchAll(PDO::FETCH_COLUMN, 0);
+		   //print_r($postdatearr);
+		   //echo "<br/><br/>";	   
+
+		   $query = "SELECT DISTINCT stationname FROM IOpoliceNPM " .$where . " order by stationname";
+		   //echo $query.'<br/>';
+		   $result = $db->query($query);
+		   $namearr = $result->fetchAll(PDO::FETCH_COLUMN, 0);
+		   //print_r($namearr);
+		   //echo "<br/><br/>";
+
+		   $array = array();
+				$totalio = array();
+		   for($i=0; $i<count($postdatearr); $i++){
+			   $array[$postdatearr[$i]] = array();
+			   $totalio[$postdatearr[$i]] = 0;
+			   for($j=0; $j<count($namearr); $j++){
+				   $array[$postdatearr[$i]][$namearr[$j]] = 0;
+			   }
 		   }
-	   }
-	
-	   //print_r($array);
-	   //echo "<br/><br/>";
-			
-		
 
-       $query = "select postdate, stationname, numio from IOpoliceNPM ".$where." order by postdate, stationname";	   
-	   //echo $query.'<br/>';
-	   //echo "<br/><br/>";
-	   $result = $db->query($query);    
-	   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-		   $cmp = strcmp($row["stationname"],"ภ.จว.นครพนม");
-		   if($cmp == 0){
-			   $num = $row["numio"]*2;
-		   }else{
-			   $num = $row["numio"];						
-		   }
-		   $array[$row["postdate"]][$row["stationname"]] += $num;
-		   $totalio[$row["postdate"]] += $num;
-		
-		   /*
-		   echo "*********************************************";
-		   print_r($row);
-		   echo "<br/><br/>";
 		   //print_r($array);
-		   echo "<br/><br/>";
-		   print_r($totalio);
-		   echo "<br/><br/>";
-		   */
-	   }
-	   //print_r($array);    	
-	   //echo "<br/><br/>";
-	   
+		   //echo "<br/><br/>";
 
-		echo "<td></td>";
-		for($i=0; $i<count($postdatearr); $i++){
-			echo "<td>". $postdatearr[$i]."</td>";
-		}
-		echo "</tr></thead>";
-			
-		
-		echo "<tbody>";
-		//print_r($namearr);
-		$stationnamearr = array("ภ.จว.นครพนม","สภ.กุตาไก้","สภ.ท่าอุเทน","สภ.ธาตุพนม","สภ.นาแก","สภ.นาโดน","สภ.นาทม","สภ.นาหว้า","สภ.บ้านกลาง","สภ.บ้านแพง","สภ.ปลาปาก","สภ.พระซอง","สภ.โพนสวรรค์","สภ.เมืองนครพนม","สภ.เรณูนคร","สภ.วังยาง","สภ.ศรีสงคราม","สภ.หนองบ่อ","สภ.หนองฮี","สภ.หลักศิลา");
-        	//$namearr is from db	   
-		for($j=0; $j<count($stationnamearr); $j++){
-			echo "<tr><td>".$stationnamearr[$j]."</td>";
-			$indexinarr = array_search($stationnamearr[$j], $namearr);
-			//echo $indexinarr;
-			if($indexinarr !== false) {
-				for($i=0; $i<count($postdatearr); $i++){
-					echo "<td>". $array[$postdatearr[$i]][$namearr[$indexinarr]]."</td>";
-				}
-				
-			}else{//not found stationname
-				for($i=0; $i<count($postdatearr); $i++){
-					echo "<td></td>";
-				}
-			}
-			echo "</tr>";
-	   	}
-		
-			echo "<tr><td>รวม</td>";
+
+
+	       $query = "select postdate, stationname, numio from IOpoliceNPM ".$where." order by postdate, stationname";	   
+		   //echo $query.'<br/>';
+		   //echo "<br/><br/>";
+		   $result = $db->query($query);    
+		   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			   $cmp = strcmp($row["stationname"],"ภ.จว.นครพนม");
+			   if($cmp == 0){
+				   $num = $row["numio"]*2;
+			   }else{
+				   $num = $row["numio"];						
+			   }
+			   $array[$row["postdate"]][$row["stationname"]] += $num;
+			   $totalio[$row["postdate"]] += $num;
+
+			   /*
+			   echo "*********************************************";
+			   print_r($row);
+			   echo "<br/><br/>";
+			   //print_r($array);
+			   echo "<br/><br/>";
+			   print_r($totalio);
+			   echo "<br/><br/>";
+			   */
+		   }
+		   //print_r($array);    	
+		   //echo "<br/><br/>";
+
+
+			echo "<td></td>";
 			for($i=0; $i<count($postdatearr); $i++){
-				echo "<td>". $totalio[$postdatearr[$i]]."</td>";
+				echo "<td>". $postdatearr[$i]."</td>";
 			}
-			echo "</tr>";
-			//print_r($totalio);
-    
-	   //$result = $db->query($query);    
-	
-	   //print_r($result->fetchAll());
-	   /*
-	   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-	   echo "<tr>";
-	   echo "<td>" . $row["postdate"] . "</td>";
-	   echo "<td>" . htmlspecialchars($row["stationname"]) . "</td>";
-	   echo "<td>" . htmlspecialchars($row["numio"]) . "</td>";
-	   echo "</tr>";
-	   }
-	   */
-	   $result->closeCursor();
+			echo "</tr></thead>";
+
+
+			echo "<tbody>";
+			//print_r($namearr);
+			$stationnamearr = array("ภ.จว.นครพนม","สภ.กุตาไก้","สภ.ท่าอุเทน","สภ.ธาตุพนม","สภ.นาแก","สภ.นาโดน","สภ.นาทม","สภ.นาหว้า","สภ.บ้านกลาง","สภ.บ้านแพง","สภ.ปลาปาก","สภ.พระซอง","สภ.โพนสวรรค์","สภ.เมืองนครพนม","สภ.เรณูนคร","สภ.วังยาง","สภ.ศรีสงคราม","สภ.หนองบ่อ","สภ.หนองฮี","สภ.หลักศิลา");
+			//$namearr is from db	   
+			for($j=0; $j<count($stationnamearr); $j++){
+				echo "<tr><td>".$stationnamearr[$j]."</td>";
+				$indexinarr = array_search($stationnamearr[$j], $namearr);
+				//echo $indexinarr;
+				if($indexinarr !== false) {
+					for($i=0; $i<count($postdatearr); $i++){
+						echo "<td>". $array[$postdatearr[$i]][$namearr[$indexinarr]]."</td>";
+					}
+
+				}else{//not found stationname
+					for($i=0; $i<count($postdatearr); $i++){
+						echo "<td></td>";
+					}
+				}
+				echo "</tr>";
+			}
+
+				echo "<tr><td>รวม</td>";
+				for($i=0; $i<count($postdatearr); $i++){
+					echo "<td>". $totalio[$postdatearr[$i]]."</td>";
+				}
+				echo "</tr>";
+				//print_r($totalio);
+
+		   //$result = $db->query($query);    
+
+		   //print_r($result->fetchAll());
+		   /*
+		   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+		   echo "<tr>";
+		   echo "<td>" . $row["postdate"] . "</td>";
+		   echo "<td>" . htmlspecialchars($row["stationname"]) . "</td>";
+		   echo "<td>" . htmlspecialchars($row["numio"]) . "</td>";
+		   echo "</tr>";
+		   }
+		   */
+		   $result->closeCursor();
 
 ?>
    </tbody>
